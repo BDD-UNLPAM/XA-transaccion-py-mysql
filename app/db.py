8ed9ba9e3c18047297e3e640c7af4f0e9b5da1bd
BDD-UNLPAM <bermudezc@ing.unlpam.edu.ar>
2025-10-07 11:55:37 -0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -20,3 +20,40 @@ def get_conn_b():
         database=os.getenv('BANK_B_DB', 'bank_b'),
         autocommit=False
     )
+
+def get_prepared_transactions():
+    """
+    Devuelve las transacciones XA PREPARED de ambos bancos.
+    """
+    results = []
+    connections = {
+        "bank_a": mysql.connector.connect(
+            host="bank_a_db",
+            user="root",
+            password="rootpass",
+            database="bank_a"
+        ),
+        "bank_b": mysql.connector.connect(
+            host="bank_b_db",
+            user="root",
+            password="rootpass2",
+            database="bank_b"
+        )
+    }
+
+    prepared_a = []
+    prepared_b = []
+
+    try:
+        cursor_a = connections["bank_a"].cursor()
+        cursor_a.execute("XA RECOVER")
+        prepared_a = [row for row in cursor_a.fetchall()]
+
+        cursor_b = connections["bank_b"].cursor()
+        cursor_b.execute("XA RECOVER")
+        prepared_b = [row for row in cursor_b.fetchall()]
+    finally:
+        for conn in connections.values():
+            conn.close()
+
+    return prepared_a, prepared_b
